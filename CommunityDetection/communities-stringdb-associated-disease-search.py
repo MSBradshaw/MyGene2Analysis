aeeaa5fca529f77ea5a74a54971b3835fb9cd876
Michael Bradshaw <michael@cu-biotedu-2-10.235.138.173.int.colorado.edu>
2020-01-24 10:08:47 -0700

    modified script for renaming nodes to allow it to run in parallel

--- CommunityDetection/communities-stringdb-associated-disease-search.py
+++ CommunityDetection/communities-stringdb-associated-disease-search.py
@@ -1,29 +1,31 @@
 import pandas as pd
 import networkx as nx
-import numpy as np
-import matplotlib.pyplot as plt
-from webweb import Web
-import obonet
 import pickle
 from os import path
-from math import sqrt
 import random
-
+import sys
 # load the pre made network of protein links
-G = pickle.load( open( "protein_links_network_named.pickle", "rb" ) )
+G = pickle.load( open( "../protein_links_network_named.pickle", "rb" ) )
 
 #load the protein information tsv to get meaning full names for the nodes
-pro_info = pd.read_csv('StringDB/protein.info.v11.0.txt',sep='\t')
+pro_info = pd.read_csv('../StringDB/protein.info.v11.0.txt',sep='\t')
 
 # make the row name the external id
 pro_info.index = pro_info.loc[:,'protein_external_id']
 
 g = G.copy()
-# give real names to the graph
-for n in G.nodes:
-    g = nx.relabel_nodes(g,{n:pro_info.loc[n,'preferred_name']})
-    break
 
-pickle.dump(g,open('protein_links_networkx_gene_named.pickle','wb'))
+name_mapping = {}
+print(sys.argv)
+start = int(sys.argv[1]) * 1000
+end = start + 1000
+# give real names to the graph
+for n in list(G.nodes)[start:end]:
+    print(n)
+    name_mapping[n] = pro_info.loc[n,'preferred_name']
+
+# g = nx.relabel_nodes(g,{n:pro_info.loc[n,'preferred_name']})
+pickle.dump(name_mapping,open('name_mapping_'+str(start/1000)+'.pickle','wb'))
+# pickle.dump(g,open('protein_links_networkx_gene_named.pickle','wb'))
 
 # search for a protein/gene's associated proteins/genes
